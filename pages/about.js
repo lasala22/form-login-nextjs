9e7f472fd1b97482df56a1441c285589a3565406
lasala22 <kenwayhuy@gmail.com>
2022-05-25 20:16:13 +0700

    login-nextjs

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
 import Layout from "./layout"
 
-function Home() {
+function About() {
   return (
     <Layout>
-      <div>Homepage</div>
+      <div>About page</div>
     </Layout>
   )
 }
 
-export default Home
+export default About
---
+++ pages/blog.js
@@ -0,0 +1,10 @@
+import Layout from "./layout";
+
+function Blog() {
+    return ( 
+        <Layout>
+            <div>Blog page</div>
+        </Layout>
+    )
+}
+export default Blog
